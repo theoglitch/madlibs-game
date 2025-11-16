080624e862e6a78a647dbf360502047235bd038b
Theo Analysis.00 <141372070+theoglitch@users.noreply.github.com>
2025-11-16 08:36:22 +0200

    Add files via upload

---
+++ Madlibs game.py
@@ -0,0 +1,25 @@
+# Madlibs game 
+# word game where you create a story 
+# by filling in the blanks with words
+
+
+adjective1 = input("Enter an adjective (description):"  )
+noun1 = input("Enter a noun (person, place, or thing): ")
+adjective2 = input("Enter another adjective: ")
+adjective3 = input("Enter one more adjective: ")
+verb1 = input("Enter a verb ending with 'ing'(action word): ")
+
+print(f"Today i  went to a {adjective1} zoo.")
+print(f"In an exhibit I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"I was {adjective3}!")
+
+
+
+
+
+
+
+
+
+
